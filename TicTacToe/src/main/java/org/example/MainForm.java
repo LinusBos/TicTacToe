0f0305cd9ddb104facf26b447a1f61cb40699d6d
Linus Boström <linus.bostrom@hotmail.com>
2023-12-01 16:35:12 +0100

    Second commit
    First version of the home screen of the game.

--- TicTacToe/src/main/java/org/example/MainForm.java
+++ TicTacToe/src/main/java/org/example/MainForm.java
@@ -1,4 +1,54 @@
 package org.example;
 
-public class MainForm {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainForm extends JFrame {
+    private JPanel MainPanel;
+    private JPanel BottomPanel;
+    private JPanel BottomRight;
+    private JPanel BottomLeft;
+    private JPanel TopPanel;
+    private JLabel GameTitle;
+    private JButton NewGameButton;
+    private JButton HighscoreButton;
+    private JButton SettingsButton;
+    private JButton GameRulesButton;
+
+    private JFrame jFrame;
+
+    public MainForm() {
+        jFrame = new JFrame();
+        jFrame.setContentPane(MainPanel);
+        jFrame.pack();
+        jFrame.setSize(400,400);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.setVisible(true);
+        NewGameButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        SettingsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        HighscoreButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        GameRulesButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 }
