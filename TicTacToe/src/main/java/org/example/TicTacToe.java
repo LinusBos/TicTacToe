e2b6d55f582071c2e00b04a63dc82df48bc96839
Martin Krook <krookmartin@yahoo.com>
2023-12-08 11:53:26 +0100

    First game commit
    Added gameplan, game logick

--- TicTacToe/src/main/java/org/example/TicTacToe.java
+++ TicTacToe/src/main/java/org/example/TicTacToe.java
@@ -1,14 +1,13 @@
 package org.example;
 
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        MainForm mainForm = new MainForm();
-
         SwingUtilities.invokeLater(() -> {
             TicTacToe game = new TicTacToe();
             game.setVisible(true);
         });
-
-
     }
 }
